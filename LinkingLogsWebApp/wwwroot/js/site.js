8a06b9afdc4d297d82d4a56665dc08aa5c0cf054
kkuop <60794860+kkuop@users.noreply.github.com>
2020-04-14 16:44:14 -0500

    beginning to scaffold identity and styling the index page for the home controller

--- LinkingLogsWebApp/wwwroot/js/site.js
+++ LinkingLogsWebApp/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function ($) {
+    $('#cardManager').hover(function () {
+        $(this).toggleClass("shadow-lg");
+    });
+    $('#cardTrucker').hover(function () {
+        $(this).toggleClass("shadow-lg");
+    });
+})(jQuery);
