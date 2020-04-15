4d1767d3bffce01932c171225bea7aa0925277ab
kkuop <60794860+kkuop@users.noreply.github.com>
2020-04-15 11:41:30 -0500

    created view for About page accessible by anonymous users

--- LinkingLogsWebApp/Controllers/HomeController.cs
+++ LinkingLogsWebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LinkingLogsWebApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
